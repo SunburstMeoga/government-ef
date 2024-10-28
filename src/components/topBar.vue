e3b90536fb95ac7b0c4c182014a15c5971805a0a
SunburstMeoga <1025113986@qq.com>
2024-10-29 04:59:14 +0800

    pages

---
+++ src/components/topBar.vue
@@ -0,0 +1,36 @@
+<route lang="json5">
+{
+  style: {
+    navigationBarTitleText: '关于',
+  },
+}
+</route>
+<template>
+  <view class="mt-200rpx">this is top bar</view>
+</template>
+
+<script setup>
+const { safeAreaInsets, statusBarHeight } = uni.getSystemInfoSync()
+
+onLoad(() => {
+  uni.getSystemInfo({
+    success: (e) => {
+      let statusBar = 0 // 状态栏高度
+      let customBar = 0 // 状态栏高度 + 导航栏高度
+      let navbar = 0 // 自定义标题与胶囊对齐高度
+
+      statusBar = e.statusBarHeight
+      const custom = wx.getMenuButtonBoundingClientRect()
+      customBar = custom.bottom + custom.top - e.statusBarHeight
+
+      navbar = (custom.top - e.statusBarHeight) * 2 + custom.height
+      //   console.log(statusBar, customBar, navbar)
+      //   barHeight.value = navbar + safeAreaInsets.top
+      //   console.log(barHeight.value)
+      console.log(safeAreaInsets, statusBarHeight)
+    },
+  })
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,27 +8,101 @@
 }
 </route>
 <template>
-  <view class="flex flex-col pt-176rpx justify-between items-center text-amber">
-    this is home page
+  <view class="flex flex-col justify-between items-center relative">
+    <view class="w-full h-screen absolute inset-0">
+      <image src="../../static/home/iPhone 13 mini_11 Pro_X (2).png"></image>
+    </view>
+
+    <view
+      class="relative z-1 flex items-center"
+      :style="`margin-top:${barHeight + 10}rpx; height: ${navbar + 6}rpx;`"
+    >
+      首页
+    </view>
+    <view class="relative z-1 mt-6rpx h-110rpx w-full flex justify-center items-end mb-28rpx">
+      <view class="bg-white rounded-24rpx w-680rpx h-88rpx flex justify-between items-center">
+        <view class="w-112rpx h-91rpx -mt-22rpx text-slate-900 ml-21rpx">
+          <image src="../../static/home/1.png" />
+        </view>
+        <view class="text-slate-900 ml-7rpx text-28rpx h-40rpx">
+          环保相关行动正在进行中，点击查看
+        </view>
+        <view class="mr-22rpx w-20rpx h-24rpx -mt-22rpx">
+          <image src="../../static/home/2.png"></image>
+        </view>
+      </view>
+    </view>
+    <view class="w-686rpx h-480rpx relative mx-auto mt-28rpx flex flex-col">
+      <view class="w-686rpx h-480rpx absolute inset-0 rounded-24rpx overflow-hidden z-1">
+        <image src="../../static/home/3.png"></image>
+      </view>
+      <view class="mt-29rpx mb-24rpx ml-32rpx w-140rpx h-186rpx relative z-4">
+        <image src="../../static/home/guanjun.png" class="absolute inset-0" />
+        <view class="w-full h-full flex flex-col items-center absolute inset-0 w-140rpx h-186rpx">
+          <view class="mt-54rpx text-20rpx text-amber text-20rpx">第一名</view>
+          <view class="text-20rpx text-slate-900 mb-10rpx text-24rpx">福田学校</view>
+          <view class="flex justify-center w-full items-center h-24rpx">
+            <view class="w-24rpx h-24rpx flex justify-center items-center">
+              <image src="../../static/home/4.png" />
+            </view>
+            <view class="text-green-600 text-24rpx h-28rpx flex items-center">100</view>
+          </view>
+        </view>
+      </view>
+      <view class="ml-42rpx w-120rpx h-124rpx relative z-4">
+        <image src="../../static/home/mall.png" />
+      </view>
+    </view>
+
+    <div class="border border-red-500 border-12rpx w-full -mt-78rpx h-120rpx relative z-4 mb-34rpx">
+      <img src="../../static/home/5.png" class="w-full h-80rpx absolute inset-0" alt="" />
+      <div class="w-full text-slate-9 text-center text-32rpx mt-88rpx mb-34rpx">
+        宝安区已经开展
+        <span class="text-amber">23</span>
+        个减污替代修复项目
+        <br />
+        年降碳量达到
+        <span class="text-amber">24</span>
+        吨
+      </div>
+      <div class="flex justify-evenly items-center w-full">
+        <div class="flex flex-col items-center" v-for="(item, index) in typeItems" :key="index">
+          <div class="flex justify-center items-center w-88rpx h-88rpx mb-24rpx">
+            <img :src="item.img" alt="" />
+          </div>
+          <div class="text-gray-500 text-28rpx">{{ item.title }}</div>
+        </div>
+      </div>
+    </div>
   </view>
 </template>
 
 <script setup>
-// import PLATFORM from '@/utils/platform'
-
-// defineOptions({
-//   name: 'Home',
-// })
-
-// 获取屏幕边界到安全区域距离
-const { safeAreaInsets } = uni.getSystemInfoSync()
-const author = ref('菲鸽')
-const description = ref(
-  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
-)
-// 测试 uni API 自动引入
+const barHeight = ref(0)
+const navbar = ref(0)
+const typeItems = ref([
+  { title: '垃圾分类', img: '../../static/home/type1.png' },
+  { title: '增值流放', img: '../../static/home/type2.png' },
+  { title: '种植树木', img: '../../static/home/type3.png' },
+  { title: '修复生态', img: '../../static/home/type4.png' },
+  { title: '低碳活动', img: '../../static/home/type5.png' },
+])
 onLoad(() => {
-  console.log(author)
+  uni.getSystemInfo({
+    success: (e) => {
+      let statusBar = 0 // 状态栏高度
+      let customBar = 0 // 状态栏高度 + 导航栏高度
+      navbar.value = 0 // 自定义标题与胶囊对齐高度
+
+      statusBar = e.statusBarHeight
+      const custom = wx.getMenuButtonBoundingClientRect()
+      customBar = custom.bottom + custom.top - e.statusBarHeight
+
+      navbar.value = (custom.top - e.statusBarHeight) * 2 + custom.height
+      barHeight.value = statusBar * 2
+      console.log(navbar.value, barHeight.value)
+    },
+  })
 })
 </script>
 
