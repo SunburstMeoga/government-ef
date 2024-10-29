1bd66ab862d562b51b2ec318b6283a8a7622d0a7
SunburstMeoga <1025113986@qq.com>
2024-10-30 02:30:11 +0800

    pages

---
+++ src/pages/index/components/product.vue
@@ -0,0 +1,15 @@
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationBarTitleText: '',
+  },
+}
+</route>
+<template>
+  <div style="border: 1px solid red" class="w-full h-600rpx">this is product card</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
