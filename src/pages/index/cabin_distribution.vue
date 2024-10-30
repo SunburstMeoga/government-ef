b6419b8b76ff7821806bc8751b2dd245212e33b1
SunburstMeoga <1025113986@qq.com>
2024-10-31 04:09:08 +0800

    pages

--- src/pages/index/cabin_distribution.vue
+++ src/pages/index/cabin_distribution.vue
@@ -1,49 +1,45 @@
-<route lang="json5" type="home">
-    {
-      layout: 'default',
-      style: {
-        navigationBarTitleText: '小屋分布',
-      },
-    }
-    </route>
 <template>
-    <view class="container">
-        <map :latitude="latitude" :longitude="longitude" :scale="16" style="width: 100%; height: 400px;"
-            :markers="markers" />
-    </view>
+  <view class="container">
+    <map
+      :latitude="latitude"
+      :longitude="longitude"
+      :scale="16"
+      style="width: 100%; height: 400px"
+      :markers="markers"
+    />
+  </view>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
-let latitude = ref(22.5865)
-let longitude = ref(113.8635)
-let markers = ref([{
+const latitude = ref(22.5865)
+const longitude = ref(113.8635)
+const markers = ref([
+  {
     id: 1,
     latitude: 22.5865,
     longitude: 113.8635,
-    iconPath: "../..//static/marker.png", // 标记点的图标
+    iconPath: '../..//static/marker.png', // 标记点的图标
     width: 30,
-    height: 30
-}])
-
-
+    height: 30,
+  },
+])
 </script>
 
 <style lang="scss" scoped>
 .container {
-
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 }
 
 button {
-    margin-top: 10px;
-    padding: 10px;
-    background-color: #007AFF;
-    color: #ffffff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  padding: 10px;
+  margin-top: 10px;
+  color: #ffffff;
+  cursor: pointer;
+  background-color: #007aff;
+  border: none;
+  border-radius: 5px;
 }
 </style>
